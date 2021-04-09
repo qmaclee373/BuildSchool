e3bdac1838f3d7b187338df729f2b457313c7a8f
qmaclee373 <qmaclee373@gmail.com>
2021-04-09 16:09:59 +0800

    0409_002

--- 040817.js
+++ 040817.js
@@ -1,12 +1,7 @@
-ffunction $g(value) {
-    //判斷是否為id selector
-    if (value.includes('#')) {
-    //回傳Element
-    return document.querySelector(value);
-    }
-    //回傳NodeList集合
-    var nodelist = document.querySelectorAll(value);
-    return nodelist.length == 1 ? nodelist[0] :
-    nodelist;
-   }
-   export { $g, displayName, $title, $ex, $log };
+let iterable = [103, 105, 107, 109, 111, 113, 115, 117];
+
+// 回傳「key」  "0", "1", "2"
+for (let i in iterable) {console.log(i);}
+
+// 回傳「value」 // 3, 5, 7
+for (let i of iterable) {console.log(i);}

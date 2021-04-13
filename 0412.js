db82c2f6b24c1604069d7b97726fb7cbec2d4d13
qmaclee373 <qmaclee373@gmail.com>
2021-04-13 14:52:47 +0800

    0413_001

---
+++ 0412.js
@@ -0,0 +1,70 @@
+/* var name = "Kevin";
+let age = 35;
+const weight = 65;
+
+{
+    var name ="Marry";
+    let age = 28;
+    const weight = 48;
+}
+
+console.log(age);
+console.log(weight); */
+/* 
+const person = {name:"Kevin",Mobile:"0935-123456"};
+undefined;
+person */
+/* 
+function displayTime(){
+
+let date = new Date();
+let h = date.getHours();
+let m = date.getMinutes();
+let s = date.getSeconds();
+console.log(`${h}:${m}:${s}<br/>`);
+}
+ */
+/* const print = function(... Args){
+    /* console.log(typeof Args);
+    console.log(Args instanceof Array); */
+   /*  console.log(Args[0]);
+    console.log(Args[1]);
+    console.log(Args[2]);
+    console.log(Args[3]);
+    // console.log(Args.join());
+   }
+   print("昔⼈已乘黃鶴去","此地空餘黃鶴樓","黃鶴⼀去不復返","白雲千載空悠悠"); 
+  */
+
+  /*  var taiwanArray = ['Taiwan',41,1,12];
+   var chinaArray = ['China',80134,2914,44595];
+   var americaArray = ['America',9665,146,694];
+
+   var map = new Map();
+
+
+   console.log(map.get('america'));
+   console.log(map.get('taiwan'));
+   console.log(map.get('china'));
+
+   map.delete("china");
+   console.log(map);
+   
+   map.clear();
+ */
+
+let set = new Set();
+
+set.add(1);
+set.add(2);
+set.add(3);
+set.add(4);
+set.add(5);
+set.add(6);
+set.add(7);
+set.add(8);
+set.add(9);
+
+console.log(set);
+console.log('size : ' + set.size)
+console.log('type : ' + typeof set);

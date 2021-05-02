769a733c966b5dc85368cb1dbadc3bcfef8e86fd
qmaclee373 <qmaclee373@gmail.com>
2021-05-02 11:18:11 +0800

    111

--- 040817.js
+++
@@ -1,7 +0,0 @@
-let iterable = [103, 105, 107, 109, 111, 113, 115, 117];
-
-// 回傳「key」  "0", "1", "2"
-for (let i in iterable) {console.log(i);}
-
-// 回傳「value」 // 3, 5, 7
-for (let i of iterable) {console.log(i);}
